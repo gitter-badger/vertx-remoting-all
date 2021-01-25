d4ada74513bbc3f6b625151dc12b0adddbcdadae
zhouhailin <zhouhailin555@aliyun.com>
2021-01-26 00:39:05 +0800

    commit : 0.1.0

---
+++ vertx-remoting-spring-boot-starter-example/src/main/java/link/thingscloud/vertx/remoting/spring/boot/starter/example/ExampleApplication.java
@@ -0,0 +1,15 @@
+package link.thingscloud.vertx.remoting.spring.boot.starter.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouhailin
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class ExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+}
